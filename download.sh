a9035da3e08018a17b4d835e685d1e0a6a2cf742
alexdelorenzo <you@example.com>
2020-05-05 20:03:06 -0400

    init

---
+++ download.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+EXCLUDE=lowlatency
+VERSION="{1:-5.6}"
+URL="https://kernel.ubuntu.com/~kernel-ppa/mainline/v$VERSION"
+
+curl -s "$URL/" \
+  | pup 'a:contains("amd64") text{}' \
+  | grep deb | sort | uniq \
+  | while read file; do
+      if ! test -e "$file" && ! [[ "$file" =~ "$EXCLUDE" ]]; then
+        wget "$URL/$file"
+      fi
+    done
+
+rm -v *$EXCLUDE*
+sudo dpkg -i *.deb
